731b6d0795b962545107d627cfb43726360491a1
Aske Lundsgaard <askelundsgaard@live.dk>
2021-04-05 18:46:17 +0200

    ts for post

--- app/build_files/HTTP/methods/post.js
+++ app/build_files/HTTP/methods/post.js
@@ -1,43 +1,39 @@
-
-
-export default async function post(requestBody, extraOrdinaryUrl) {
-
-  const urlObj = {
-    'sales': this.salesUrl,
-    'shopping': this.shoppingUrl
-  }
-
-  const url = extraOrdinaryUrl ? urlObj[extraOrdinaryUrl] : this.testUrl;
-  
-  const body = extraOrdinaryUrl ? 
-  requestBody : 
-  JSON.stringify({
-    query: requestBody.query,
-    variables: requestBody.variables,
-  });
-
-  //console.log
-  
-  const response = await fetch(url, {
-   // signal: signal,
-    method: 'POST',
-    //mode: 'no-cors',
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': 'Bearer ' + requestBody?.variables?.token
-    },
-    body: body
-  })
-
-  const jsonData = await response.json();
-
-  if (response.ok && response.status === 200) {
-     // console.log(jsonData);
-    return jsonData;
-  } else {
-    console.log(jsonData)
-    let err = jsonData.errors.reduce((a, n) => a + ' ' + n.message, '');
-    console.error(jsonData)
-    throw new Error(err)
-  }
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+async function post(requestBody, extraOrdinaryUrl) {
+    var _a;
+    const urlObj = {
+        'sales': this.salesUrl,
+        'shopping': this.shoppingUrl
+    };
+    const url = extraOrdinaryUrl ? urlObj[extraOrdinaryUrl] : this.testUrl;
+    const body = extraOrdinaryUrl ?
+        requestBody :
+        JSON.stringify({
+            query: requestBody.query,
+            variables: requestBody.variables,
+        });
+    //console.log
+    const response = await fetch(url, {
+        // signal: signal,
+        method: 'POST',
+        //mode: 'no-cors',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + ((_a = requestBody === null || requestBody === void 0 ? void 0 : requestBody.variables) === null || _a === void 0 ? void 0 : _a.token)
+        },
+        body: body
+    });
+    const jsonData = await response.json();
+    if (response.ok && response.status === 200) {
+        // console.log(jsonData);
+        return jsonData;
+    }
+    else {
+        console.log(jsonData);
+        let err = jsonData.errors.reduce((a, n) => a + ' ' + n.message, '');
+        console.error(jsonData);
+        throw new Error(err);
+    }
 }
+exports.default = post;
